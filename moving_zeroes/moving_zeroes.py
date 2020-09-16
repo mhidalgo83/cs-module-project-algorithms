4134aa522f864f8a0dd4bc22e3192231c91bd475
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-09-16 15:20:40 -0700

    sliding window complete

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,6 +2,9 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+new_arr = [0, 0, 0, 0, 3, 2, 1]
+
 def moving_zeroes(arr):
     # Your code here
     new_list = []
@@ -13,6 +16,13 @@ def moving_zeroes(arr):
             zero_list.append(arr[i])
     return new_list + zero_list
 
+    # for i in range(len(arr)):
+    #     if arr[i] == 0:
+    #         arr.append(arr[i])
+    #         arr.pop(i)
+    # return arr
+
+print(moving_zeroes(new_arr))
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,16 +1,34 @@
 '''
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
-'''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+'''
+# arr = [70, 37, 100, 66, 1, 45, 27, 62, 75, 57, 92, 66, 9, 39, 15, 69, 46, 72]
+
+def sliding_window_max(nums, k):
+    max_list = []
+    start = 0
+    end = k
+    count = 0
+    max_val = min(nums)
+    while count < (len(nums) - k + 1):
+        for i in nums[start:end]:
+            if max_val < i:
+                max_val = i
+        max_list.append(max_val)
+        start += 1
+        end += 1
+        count +=1
+        max_val = min(nums)
+    return max_list
+
+
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
+    # arr = [1, 3, -1, -3, 5, 3, 6, 7]
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
-    k = 3
+    k = 2
 
     print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
