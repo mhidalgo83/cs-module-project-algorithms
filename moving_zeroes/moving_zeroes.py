8586b44b46e6c12a53ba36d0dbc379491fe7f0cd
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-09-17 14:37:01 -0700

    refactored moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,26 +3,23 @@ Input: a List of integers
 Returns: a List of integers
 '''
 
-new_arr = [0, 0, 0, 0, 3, 2, 1]
+
 
 def moving_zeroes(arr):
     # Your code here
-    new_list = []
-    zero_list = []
-    for i in range(len(arr)):
-        if arr[i] != 0:
-            new_list.append(arr[i])
-        else:
-            zero_list.append(arr[i])
-    return new_list + zero_list
-
+    # First pass solution:
+    # new_list = []
+    # zero_list = []
     # for i in range(len(arr)):
-    #     if arr[i] == 0:
-    #         arr.append(arr[i])
-    #         arr.pop(i)
-    # return arr
+    #     if arr[i] != 0:
+    #         new_list.append(arr[i])
+    #     else:
+    #         zero_list.append(arr[i])
+    # return new_list + zero_list
 
-print(moving_zeroes(new_arr))
+    # Refactored code, implemented in one line
+    return [n for n in arr if n != 0] + [n for n in arr if n == 0]
+
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
